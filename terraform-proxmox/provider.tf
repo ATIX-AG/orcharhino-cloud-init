8dc4112e7f66e211e6908b3b4f0476d335c7c29a
Jan Löser <loeser@atix.de>
2023-09-29 11:42:11 +0200

    Format according to `terraform fmt`

--- terraform-proxmox/provider.tf
+++ terraform-proxmox/provider.tf
@@ -6,21 +6,21 @@ terraform {
     }
   }
 
-#  # Gitlab backend
-#  backend "http" {
-#    address = "https://{GITLAB_URL}/api/v4/projects/{PROJECT_ID}/terraform/state/main"
-#    lock_address = "https://{GITLAB_URL}/api/v4/projects/{PROJECT_ID}/terraform/state/main/lock"
-#    unlock_address = "https://{{GITLAB_URL}}/api/v4/projects/{PROJECT_ID}/terraform/state/main/lock"
-#    lock_method = "POST"
-#    unlock_method = "DELETE"
-#  }
+  #  # Gitlab backend
+  #  backend "http" {
+  #    address = "https://{GITLAB_URL}/api/v4/projects/{PROJECT_ID}/terraform/state/main"
+  #    lock_address = "https://{GITLAB_URL}/api/v4/projects/{PROJECT_ID}/terraform/state/main/lock"
+  #    unlock_address = "https://{{GITLAB_URL}}/api/v4/projects/{PROJECT_ID}/terraform/state/main/lock"
+  #    lock_method = "POST"
+  #    unlock_method = "DELETE"
+  #  }
 
-#  # AWS S3 backend
-#  backend "s3" {
-#    bucket = "mybucket"
-#    key    = "path/to/my/key"
-#    region = "us-east-1"
-#  }
+  #  # AWS S3 backend
+  #  backend "s3" {
+  #    bucket = "mybucket"
+  #    key    = "path/to/my/key"
+  #    region = "us-east-1"
+  #  }
 
 }
 
